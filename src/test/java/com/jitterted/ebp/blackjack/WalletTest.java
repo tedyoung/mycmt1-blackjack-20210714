69c14b248c33ca597f1475e2dd6781620ce5fe2d
Ted M. Young <tedyoung@gmail.com>
2021-07-15 14:54:18 -0700

    TDD: Added addMoney & balance behavior to wallet.

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -24,4 +24,36 @@ public class WalletTest {
                 .isFalse();
     }
 
+    @Test
+    public void newWalletHasZeroBalance() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.balance())
+                .isZero();
+    }
+
+    @Test
+    public void newWalletAddMoneyOf10HasBalanceOf10() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(10);
+
+        assertThat(wallet.balance())
+                .isEqualTo(10);
+    }
+
+    @Test
+    public void newWalletAdd7AndAdd8ThenBalanceIs15() throws Exception {
+        // GIVEN a new wallet
+        Wallet wallet = new Wallet();
+
+        // WHEN add 7
+        wallet.addMoney(7);
+        // and  add 8
+        wallet.addMoney(8);
+
+        // THEN balance is 15
+        assertThat(wallet.balance())
+                .isEqualTo(7 + 8); // EVIDENT DATA
+    }
 }
