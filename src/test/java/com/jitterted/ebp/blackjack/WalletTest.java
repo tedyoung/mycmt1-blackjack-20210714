30b2c40391edb2eac9a24729ec4a408c5c68ba72
Ted M. Young <tedyoung@gmail.com>
2021-07-15 15:05:28 -0700

    TDD: Added handling addMoney with 0 or negative which is not allowed

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -56,4 +56,22 @@ public class WalletTest {
         assertThat(wallet.balance())
                 .isEqualTo(7 + 8); // EVIDENT DATA
     }
+
+    @Test
+    public void addMoneyOfZeroThrowsException() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThatThrownBy(() -> {
+            wallet.addMoney(0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void addNegativeMoneyThrowsException() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThatThrownBy(() -> {
+            wallet.addMoney(-1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
